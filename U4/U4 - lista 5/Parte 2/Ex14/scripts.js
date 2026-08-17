60f33b042ecfa6377a085076312590cb44734f75
Gamahen011 <ricaohgp@gmail.com>
2026-08-17 16:41:21 -0300

    14

--- U4/U4 - lista 5/Parte 2/Ex14/scripts.js
+++ U4/U4 - lista 5/Parte 2/Ex14/scripts.js
@@ -1,40 +1,31 @@
-let botoes
-let divs 
+let botoes = [0, 1, 2, 3]
+let divs = [0, 1, 2, 3]
 
 for (let i = 0; i < 4; i++) {
   botoes[i] = document.getElementById(`btn${i+1}`);
   divs[i] = document.getElementById(`box${i+1}`);
-  botoes[i].addEventListener('click', () => atualizar(divs[i+1]));
+  botoes[i].addEventListener('click', () => atualizar(divs[i]));
 }
 
-function atualizar(botao) {
-    switch (botao) {
-        case btn1:
-
+function atualizar(div) {
+    switch(div) {
+        case divs[0]:
+            div.style.backgroundColor = "lightblue";
+            div.style.transform = "rotate(10deg)";
+            break;
+        case divs[1]:
+            div.style.backgroundColor = "lightgreen";
+            div.style.borderRadius = "100%";
+            break;
+        case divs[2]:
+            div.style.backgroundColor = "#FFCCCC";
+            div.style.paddingLeft = "20px";
+            div.style.paddingRight = "20px";
+            break;
+        case divs[3]:
+            div.style.backgroundColor = "lightyellow";
+            div.style.paddingTop = "20px";
+            div.style.paddingBottom = "20px";
+            break;
     }
-}
-
-
-
-
-
-function mudarDiv(div) {
-  if (div == divs[0]) {
-    div.style.backgroundColor = "lightblue";
-    div.style.transform = "rotate(10deg)"
-  }
-  else if (div == divs[1]) {
-    div.style.backgroundColor = "lightgreen";
-    div.style.borderRadius = "100%";
-  }
-  else if (div == divs[2]) {
-    div.style.backgroundColor = "#FFCCCC";
-    div.style.paddingLeft = "20px";
-    div.style.paddingRight = "20px";
-  }
-  else {
-    div.style.backgroundColor = "lightyellow";
-    div.style.paddingTop = "20px";
-    div.style.paddingBottom = "20px";
-  }
 }
