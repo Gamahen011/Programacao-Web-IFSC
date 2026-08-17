08c2c33dc15f5a38b27e7fe8c917afdcc535cbe7
Gamahen011 <ricaohgp@gmail.com>
2026-08-17 17:21:04 -0300

    15

--- U4/U4 - lista 5/Parte 2/Ex15/scripts.js
+++ U4/U4 - lista 5/Parte 2/Ex15/scripts.js
@@ -1,31 +1,68 @@
-let botoes = [0, 1, 2, 3]
-let divs = [0, 1, 2, 3]
+const listaProdutos = [1, 2, 3];
+const itens = document.getElementById("itensCarrinho");
+const limpar = document.getElementById("limparCarrinho");
+const inputFrete = document.getElementById("cep");
+const botaoFrete = document.getElementById("calcularFrete");
+const valorFrete = document.getElementById("valorFrete");
+const inputCupom = document.getElementById("cupom");
+const botaoCupom = document.getElementById("aplicarCupom");
+const pCupom = document.getElementById("cupomAplicado");
+const totalParagrafo = document.getElementById("totalCarrinho");
 
-for (let i = 0; i < 4; i++) {
-  botoes[i] = document.getElementById(`btn${i+1}`);
-  divs[i] = document.getElementById(`box${i+1}`);
-  botoes[i].addEventListener('click', () => atualizar(divs[i]));
+listaProdutos.forEach(i => {
+  const produto = document.querySelector(`[data-id="${i}"]`);
+  let botao = produto.querySelector('.adicionar-carrinho')
+  botao.addEventListener('click', () => adicionarCarrinho(produto))
+})
+
+limpar.addEventListener('click', () => limparCarrinho());
+botaoFrete.addEventListener('click', () => calcularFrete(Number(inputFrete.value)));
+botaoCupom.addEventListener('click', () => ativarCupom(cupom.value));
+
+function calcularFrete(input) {
+  valorFrete.textContent = `${Math.trunc(input/10000000)},00`;
+  return Math.trunc(input/10000000);
 }
 
-function atualizar(div) {
-    switch(div) {
-        case divs[0]:
-            div.style.backgroundColor = "lightblue";
-            div.style.transform = "rotate(10deg)";
-            break;
-        case divs[1]:
-            div.style.backgroundColor = "lightgreen";
-            div.style.borderRadius = "100%";
-            break;
-        case divs[2]:
-            div.style.backgroundColor = "#FFCCCC";
-            div.style.paddingLeft = "20px";
-            div.style.paddingRight = "20px";
-            break;
-        case divs[3]:
-            div.style.backgroundColor = "lightyellow";
-            div.style.paddingTop = "20px";
-            div.style.paddingBottom = "20px";
-            break;
+function calcularTotal() {
+  let total = 0;
+  const itensP = itens.querySelectorAll("p");
+  itensP.forEach(item => {
+    total += item.getAttribute('data-preco');
+  });
+  if (calcularFrete()) {total += calcularFrete()}
+  if (pCupom.style.display == "inline-block") {total = total*0.9}
+  totalParagrafo.textContent = total;
+}
+
+function ativarCupom(input) {
+  if (input == "DESCONTO10") {
+    pCupom.style.display = "inline-block";
+    return true;
+  }
+}
+
+function adicionarCarrinho(produto) {
+  let novoItem = document.createElement("p");
+  let valor = produto.getAttribute('data-preco');
+  let nome = produto.getAttribute('data-nome');
+  let qnt = 1;
+  const itensP = itens.querySelectorAll("p");
+  itensP.forEach(item => {
+    if (item.getAttribute('data-preco') == valor) {
+      qnt = Number(item.getAttribute('data-qnt'))+1;
+      item.textContent = `${nome}, Quantidade: ${qnt}, Valor: R$${valor}`;
+      item.setAttribute("data-qnt", qnt);
     }
+  });
+  if (qnt == 1) {
+    novoItem.textContent = `${nome}, Quantidade: ${qnt}, Valor: R$${valor}`;
+    itens.appendChild(novoItem);
+    novoItem.setAttribute("data-preco", valor);
+    novoItem.setAttribute("data-qnt", qnt);
+  }
+}
+
+function limparCarrinho() {
+  itens.innerHTML = '';
 }
